e025ee45efd7cfd6e25280631084128a6385cdd3
justinexner <92640370+justinexner@users.noreply.github.com>
2023-09-06 19:46:23 +0200

    feat: highlighting navbar

--- src/components/navigationmenu.tsx
+++ src/components/navigationmenu.tsx
@@ -13,11 +13,22 @@ import {
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 import { ModeToggle } from "@/components/modeToggle"
+import { usePathname } from 'next/navigation';
 
 export function NavMenu(){
+  const currentRoute = usePathname();
+
+  function checkRoute(route: string) {
+    if (route === currentRoute) {
+      return "border-b-4 border-amber-400"
+    }else{
+      return ""
+    }
+  }
+
   return(
     <NavigationMenu>
-      <NavigationMenuList className="flex flex-row w-screen p-2">
+      <NavigationMenuList className="flex flex-row w-screen p-1">
         <NavigationMenuItem className="grow flex">
           <Link href="/" className="text-xl font-bold w-1/3 flex">
             <div className="mx-auto">
@@ -25,14 +36,14 @@ export function NavMenu(){
             </div>
           </Link>
         </NavigationMenuItem>
-        <div className="flex flex-row basis-1/4 space-x-4">
-          <NavigationMenuItem>
-          <Link href="/" legacyBehavior passHref>
+        <div className="flex flex-row basis-1/4 space-x-6">
+          <NavigationMenuItem className={checkRoute("/")}>
+            <Link href="/" legacyBehavior passHref>
               <NavigationMenuLink className={`text-lg ${navigationMenuTriggerStyle()}`}>Home</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
-          <NavigationMenuItem>
-          <Link href="/projects" legacyBehavior passHref>
+          <NavigationMenuItem className={checkRoute("/projects")}>
+            <Link href="/projects" legacyBehavior passHref>
               <NavigationMenuLink className={`text-lg ${navigationMenuTriggerStyle()}`}>Projects</NavigationMenuLink>
             </Link>
           </NavigationMenuItem>
