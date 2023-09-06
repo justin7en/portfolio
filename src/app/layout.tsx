cdac3515ec0ffc73de290cbea42374cce57eabc9
justinexner <92640370+justinexner@users.noreply.github.com>
2023-09-06 20:17:44 +0200

    refactor: file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/themeProvider"
 import { NavMenu } from '@/components/navigationmenu'
 
 const inter = Inter({ subsets: ['latin'] })
